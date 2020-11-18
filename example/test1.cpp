6d5f49af83550c7b99728786fe9d02bd6f65274e
KK <ery.eden@gmail.com>
2020-11-19 00:49:32 +0900

    Add some 3rds and a example code.

---
+++ example/test1.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ery on 2020/11/19.
+//
+
+#include <iostream>
+#include "box2d/box2d.h"
+
+int main(){
+    std::cout << "This is the first step to the Taga's grate work." << std::endl;
+    return 0;
+}
